8960ced882dc700ad57499018ba91a6525f8fc10
rajatrar <43917713+rajatrar@users.noreply.github.com>
2025-05-21 17:02:05 +0000

    added changes

---
+++ project-1/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import {Header} from './Components/Header'
+import { Try } from './Components/Try'
+
+function App() {
+ 
+
+  return (
+    <>
+    {/* <Header/> */}
+    <Try/>
+    </>
+  )
+}
+
+export default App

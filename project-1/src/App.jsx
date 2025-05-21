1632fa10bf37c59d9bc61a0b65aef6bf343ec5c0
rajatp00432 <rajatp00432@gmail.com>
2025-05-21 17:16:40 +0000

    new changes added

--- project-1/src/App.jsx
+++ project-1/src/App.jsx
@@ -1,14 +1,12 @@
-import './App.css'
-import {Header} from './Components/Header'
-import { Try } from './Components/Try'
+
+
 
 function App() {
  
 
   return (
     <>
-    {/* <Header/> */}
-    <Try/>
+   
     </>
   )
 }
---
+++ project-1/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Header() {
+  return (
+    <div>
+      {/* Add responsive CSS classes here */}
+      <div className="responsive-header">My Restaurant Name</div>
+    </div>
+  );
+}
+
+export default Header;
